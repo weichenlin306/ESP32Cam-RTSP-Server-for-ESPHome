f7d5a19f1742b5301801ce4fe0006dcc89abbd4a
Weichen Lin <weichenlin306@gmail.com>
2024-10-22 22:40:52 +0800

    optimized camera sensors update

--- esp32cam-rtsp-server.h
+++ esp32cam-rtsp-server.h
@@ -55,14 +55,22 @@ public:
     s->set_brightness(s, _brightness);    // -2 ~ 2
     s->set_contrast(s, _contrast);        // -2 ~ 2
     s->set_saturation(s, _saturation);    // -2 ~ 2
+    s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
     s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
     s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+    s->set_wb_mode(s, 4);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
     s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
     s->set_aec2(s, 1);           // 0 = disable , 1 = enable
-    s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+    s->set_ae_level(s, 0);       // -2 to 2
+    s->set_aec_value(s, 300);    // 0 to 1200
+    s->set_gain_ctrl(s, 0);      // 0 = disable , 1 = enable
+    s->set_agc_gain(s, 7);       // 0 to 30
+    s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+    s->set_bpc(s, 0);            // 0 = disable , 1 = enable
     s->set_wpc(s, 1);            // 0 = disable , 1 = enable
     s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
     s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+    s->set_dcw(s, 1);            // 0 = disable , 1 = enable
 
     rtspServer = WiFiServer(rtsp_port, max_client_count);
     rtspServer.begin(rtsp_port);
